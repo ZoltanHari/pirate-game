4dd1e74d9ba3881d982acfba93a4244e783c2ece
ati hun <harizozi1009@gmail.com>
2026-09-11 07:58:36 -0600

    Flip Enemy Based on Direction

--- scripts/Slime.cs
+++ scripts/Slime.cs
@@ -9,21 +9,27 @@ public partial class Slime : Node2D
     [Export] private RayCast2D _rayCastLeft;
     [Export] private RayCast2D _rayCastRight;
 
+    private AnimatedSprite2D AnimatedSprite2D;
+
  public override void _Ready()
     {
         _rayCastLeft = GetNode<RayCast2D>("RayCast2DLeft");
         _rayCastRight = GetNode<RayCast2D>("RayCast2DRight");
+        AnimatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
     }
 
     public override void _PhysicsProcess(double delta)
     {
+        AnimatedSprite2D.FlipH = direction < 0;
+
         if (direction == 1 && _rayCastRight.IsColliding())
         {
             direction = -1; 
         }
         else if (direction == -1 && _rayCastLeft.IsColliding())
         {
-            direction = 1;  
+            direction = 1;
+              
         }
 
         Position += new Vector2(direction * Speed * (float)delta, 0);
